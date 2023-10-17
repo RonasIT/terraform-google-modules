dc0b4c25cd1e704a0f4ef8c1254713f81f70cf25
Evgeny Leonov <eleonov@ronasit.com>
2023-10-17 14:48:26 +0200

    chore(iam): bump module version

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -30,7 +30,7 @@ module "teamlead" {
 
 module "developer" {
   source       = "terraform-google-modules/iam/google//modules/custom_role_iam"
-  version      = "~>7.6.0"
+  version      = "~>7.7.0"
   target_level = "project"
   target_id    = var.project_id
   role_id      = "developer"
