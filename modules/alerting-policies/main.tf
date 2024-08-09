b0e5c1eff79a6bb85a548859827a263828643483
Aleksei Kiyashkin <akiyashkin@ronasit.com>
2024-08-09 18:10:53 +0000

    feat: parametrize everything in the alerting module

--- modules/alerting-policies/main.tf
+++ modules/alerting-policies/main.tf
@@ -1,29 +1,33 @@
 resource "google_monitoring_alert_policy" "alert_policies" {
-  count = length(var.alerting_types)
+  count = length(var.alert_policies)
 
   project = var.project_id
 
-  display_name = "${var.alerting_types[count.index]} alert policy"
+  display_name = var.alert_policies[count.index].display_name
   combiner     = "OR"
 
   conditions {
-    display_name = "${var.alerting_types[count.index]} condition"
+    display_name = "${var.alert_policies[count.index].display_name} condition"
     condition_threshold {
-      filter          = "metric.type=\"${var.alerting_types[count.index]}\" AND resource.type=\"${var.alerting_types[count.index] == "pubsub.googleapis.com/subscription/oldest_unacked_message_age" ? "pubsub_subscription" : "gce_instance"}\""
-      duration        = var.durations[var.alerting_types[count.index]]
-      comparison      = "COMPARISON_GT"
-      threshold_value = var.thresholds[var.alerting_types[count.index]]
+      filter = join(" AND ", flatten([
+        "metric.type=\"${var.alert_policies[count.index].metric_type}\"",
+        "resource.type=\"${var.alert_policies[count.index].resource_type}\"",
+        [for k, v in var.alert_policies[count.index].filter_labels : "resource.label.${k}=\"${v}\""]
+      ]))
+
+      duration        = var.alert_policies[count.index].duration
+      comparison      = var.alert_policies[count.index].comparison
+      threshold_value = var.alert_policies[count.index].threshold
 
       aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_MEAN"
+        alignment_period   = var.alert_policies[count.index].alignment_period
+        per_series_aligner = var.alert_policies[count.index].per_series_aligner
       }
     }
   }
+
   notification_channels = [for email in var.emails : google_monitoring_notification_channel.email_channels[email].name]
 }
-
-
 
 resource "google_monitoring_notification_channel" "email_channels" {
   for_each = toset(var.emails)
--- modules/alerting-policies/variables.tf
+++ modules/alerting-policies/variables.tf
@@ -1,29 +1,29 @@
+variable "alert_policies" {
+  description = "A list of alert policies, each containing conditions, thresholds, durations, and other settings."
+  type = list(object({
+    display_name       = string
+    metric_type        = string
+    resource_type      = string
+    filter_labels      = map(string)
+    threshold          = number
+    duration           = string
+    comparison         = string
+    alignment_period   = string
+    per_series_aligner = string
+  }))
+}
+
 variable "project_id" {
   description = "The ID of the project where the alert policies will be created."
-  type        = string
-}
-
-variable "alerting_types" {
-  description = "A list of metric types to create alert policies for."
-  type        = list(string)
-}
-
-variable "thresholds" {
-  description = "A map where each key is a metric type and the value is the threshold for the alert."
-  type        = map(number)
-}
-
-variable "durations" {
-  description = "A map where each key is a metric type and the value is the duration for the alert."
-  type        = map(string)
-}
-
-variable "region" {
-  description = "The region or zone where the resources are located."
   type        = string
 }
 
 variable "emails" {
   description = "A list of email addresses to send notifications to."
   type        = list(string)
+}
+
+variable "region" {
+  description = "The region or zone where the resources are located."
+  type        = string
 }
